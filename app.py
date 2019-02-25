b036d87a87e2629ab04c26baaf6ad55faf2544d7
Cynthia <ivyc8192@gmail.com>
2019-02-25 14:55:56 -0800

    added test for PATCH and DELETE

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from models import db, connect_db, Cupcake
 
 app= Flask(__name__)
 app.config['SECRET_KEY'] = 'oh-so-secret'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///cupcakes-apps'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///cupcakes-app'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 

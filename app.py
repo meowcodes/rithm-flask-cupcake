bdc77f29f578a40e537d3a219aefcd05db0fb37b
meowcodes <meowcodes@gmail.com>
2019-02-25 12:20:12 -0800

    add update and delete route

--- app.py
+++ app.py
@@ -49,8 +49,6 @@ def create_cupcake():
         "rating": 5,
         "image": https://i.pinimg.com/236x/a4/8d/fc/a48dfc9b66a9ab500590832b940b3b10--icing-for-cupcakes-giant-cupcake-cakes.jpg?b=t}
     }
-
-
     """
 
     flavor = request.json("flavor")
@@ -72,3 +70,48 @@ def create_cupcake():
                           "image": new_cupcake.image}
 
     return jsonify(response=serialized_cupcake)
+
+
+@app.route('/cupcakes/<int:cupcake_id>', methods=["PATCH"])
+def update_cupcake(cupcake_id):
+    """ updates and returns updated cupcake
+
+    {response:
+        {"id": 1,
+        "flavor": chocolate,
+        "size": enormous,
+        "rating": 5,
+        "image": https://i.pinimg.com/236x/a4/8d/fc/a48dfc9b66a9ab500590832b940b3b10--icing-for-cupcakes-giant-cupcake-cakes.jpg?b=t}
+    }
+    """
+
+    cupcake = Cupcake.query.get(cupcake_id)
+
+    cupcake.flavor = request.json("flavor")
+    cupcake.size = request.json("size")
+    cupcake.rating = request.json("rating")
+    cupcake.image = request.json("image") or None
+
+    db.session.commit()
+
+    updated_cupcake = {"id": cupcake.id,
+                       "flavor": cupcake.flavor,
+                       "size": cupcake.size,
+                       "rating": cupcake.rating,
+                       "image": cupcake.image}
+
+    return jsonify(response=updated_cupcake)
+
+@app.route('/cupcakes/<int:cupcake_id>', methods=["DELETE"])
+def delete_cupcake(cupcake_id):
+    """ Deletes cupcake
+    
+    {message: "Deleted"}
+    """
+
+    cupcake = Cupcake.query.get(cupcake_id)
+    
+    db.session.delete(cupcake)
+    db.commit()
+
+    return jsonify(message="Deleted")
